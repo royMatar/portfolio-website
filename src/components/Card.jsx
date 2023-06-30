50668ffc0f8cf8875dfb91815ac8fbdc6ebf73c5
Roy <roy.mattar@outlook.com>
2023-06-30 04:21:57 +0300

    Components added

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { VStack, HStack, Heading, Text, Image } from "@chakra-ui/react";
+
+
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <VStack
+      color="black"
+      backgroundColor="white"
+      cursor="pointer"
+      borderRadius="xl"
+    >
+      <Image borderRadius="xl" src={imageSrc} alt={title} />
+      <VStack spacing={4} p={4} alignItems="flex-start">
+        <HStack justifyContent="space-between" alignItems="center">
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+        </HStack>
+        <Text color="#64748b" fontSize="lg">
+          {description}
+        </Text>
+        <HStack spacing={2} alignItems="center">
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default Card;
